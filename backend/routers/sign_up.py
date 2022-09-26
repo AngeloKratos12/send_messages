19ae933d785095abc781f14a56072b4672f2bc83
AngeloKratos12 <miharimananaangelo@yahoo.com>
2022-09-27 01:54:05 +0300

    sign up

--- backend/routers/sign_up.py
+++ backend/routers/sign_up.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="/home/n_kratos/send_messages/static"), name="public")
+templates = Jinja2Templates(directory="/home/n_kratos/send_messages/backend/templates")
 
 
 router = APIRouter(
@@ -6,8 +14,8 @@ router = APIRouter(
 )
 
 @router.get("/", summary="Authentification")
-def sign_up():
-    return {"me":"My information"}
+def sign_up(request: Request):
+    return  templates.TemplateResponse("newcompte.html", {"request":request})
 
 @router.get("/validation", summary="Mots de passe oublier")
 def forget_pass():
